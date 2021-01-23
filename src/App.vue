6933ce7dc46d5b9f525e1a79082b1cf75929b6a5
unknown <sanvicola2000@gmail.com>
2021-01-23 08:04:29 +0100

    CSS refactoring

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,78 @@
 <template>
-  <section :class="{ 'light-theme': lightMode }">
-    <section class="rest-of-content">
-      <div class="todo-content">
-        <div class="header">
-          <h3>Todo</h3>
-          <button class="theme-switcher" @click="toggleTheme">
-            <span v-if="!lightMode"
-              ><img src="../src/assets/images/icon-sun.svg" alt="Light Mode"
-            /></span>
-            <span v-else
-              ><img src="../src/assets/images/icon-moon.svg" alt="Dark Mode"
-            /></span>
-          </button>
-        </div>
-        <form action="" @submit.prevent="addGoal">
-          <div class="input-field">
-            <span class="circle"></span>
-            <input
-              type="text"
-              name="goal"
-              id=""
-              placeholder="Currently Typing"
-              v-model="newGoalValue"
-            />
-          </div>
-        </form>
-        <div class="todos-container">
-          <div class="main-content" v-if="goals.length > 0">
-            <ul>
-              <TodoItem
-                v-for="(goal, index) in filteredGoals"
-                :key="goal.name"
-                :name="goal.name"
-                :class="{ completed: goal.completed }"
-                @delete="deleteGoal(index)"
-                @toggle="toggleGoal(goal)"
-              />
-            </ul>
-            <div v-if="filteredGoals.length === 0">
-              <p>You don't have any goal in this section</p>
-            </div>
-            <div class="todos-footer">
-              <span class="items-left"
-                >{{ remainingGoals.length }} items left</span
-              >
-              <div class="filter">
-                <input
-                  type="radio"
-                  v-model="selectedFilter"
-                  value="All"
-                  id="All"
-                />
-                <label for="All">All</label>
-                <input
-                  type="radio"
-                  v-model="selectedFilter"
-                  value="Active"
-                  id="Active"
-                />
-                <label for="Active">Active</label>
-                <input
-                  type="radio"
-                  v-model="selectedFilter"
-                  value="Completed"
-                  id="Completed"
-                />
-                <label for="Completed">Completed</label>
-              </div>
-              <div class="clear">
-                <button @click="clearCompleted">clear completed</button>
-              </div>
-            </div>
-          </div>
-          <p v-else>You have not added any goals</p>
-        </div>
+  <section class="rest-of-content" :class="{ 'light-theme': lightMode }">
+    <div class="todo-content">
+      <div class="header">
+        <h3>Todo</h3>
+        <button class="theme-switcher" @click="toggleTheme">
+          <span v-if="!lightMode"
+            ><img src="../src/assets/images/icon-sun.svg" alt="Light Mode"
+          /></span>
+          <span v-else
+            ><img src="../src/assets/images/icon-moon.svg" alt="Dark Mode"
+          /></span>
+        </button>
       </div>
-    </section>
+      <form @submit.prevent="addGoal">
+        <div class="input-field">
+          <span class="circle"></span>
+          <input
+            type="text"
+            name="goal"
+            placeholder="Currently Typing"
+            v-model="newGoalValue"
+          />
+        </div>
+      </form>
+      <div class="todos-container">
+        <div class="main-content" v-if="goals.length > 0">
+          <ul>
+            <TodoItem
+              v-for="(goal, index) in filteredGoals"
+              :key="goal.name"
+              :name="goal.name"
+              :class="{ completed: goal.completed }"
+              @delete="deleteGoal(index)"
+              @toggle="toggleGoal(goal)"
+            />
+          </ul>
+          <div v-if="filteredGoals.length === 0">
+            <p>You don't have any goal in this section</p>
+          </div>
+          <div class="todos-footer">
+            <span class="items-left"
+              >{{ remainingGoals.length }} items left</span
+            >
+            <div class="filter">
+              <input
+                type="radio"
+                v-model="selectedFilter"
+                value="All"
+                id="All"
+              />
+              <label for="All">All</label>
+              <input
+                type="radio"
+                v-model="selectedFilter"
+                value="Active"
+                id="Active"
+              />
+              <label for="Active">Active</label>
+              <input
+                type="radio"
+                v-model="selectedFilter"
+                value="Completed"
+                id="Completed"
+              />
+              <label for="Completed">Completed</label>
+            </div>
+            <div class="clear">
+              <button @click="clearCompleted">clear completed</button>
+            </div>
+          </div>
+        </div>
+        <p v-else>You have not added any goals</p>
+      </div>
+    </div>
   </section>
 </template>
 
@@ -147,7 +144,7 @@ export default {
       localStorage.setItem("goals", JSON.stringify(this.goals));
     },
     clearCompleted() {
-      this.goals = this.goals.filter(goal => goal.completed === false);
+      this.goals = this.goals.filter(goal => !goal.completed);
       localStorage.setItem("goals", JSON.stringify(this.goals));
     },
     toggleTheme() {
@@ -156,13 +153,13 @@ export default {
   },
   computed: {
     remainingGoals() {
-      return this.goals.filter(goal => goal.completed === false);
+      return this.goals.filter(goal => !goal.completed);
     },
     filteredGoals() {
       if (this.selectedFilter === "All") {
         return this.goals;
       } else if (this.selectedFilter === "Active") {
-        return this.goals.filter(goal => goal.completed === false);
+        return this.goals.filter(goal => !goal.completed);
       } else if (this.selectedFilter === "Completed") {
         return this.goals.filter(goal => goal.completed);
       }
